b4a9d0c23dcc8bcb27dd453582abe8ec2a34cfd6
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-27 16:59:57 -0400

    Created a basic result type for error handling.

--- include/kirho/kirho.hpp
+++ include/kirho/kirho.hpp
@@ -1,3 +1,98 @@
 #pragma once
 
-inline void get_number() { return 42; }
+#include <iostream>
+#include <optional>
+
+namespace kirho
+{
+    template<typename T>
+    concept printable = requires(T a)
+    {
+        { std::cout << a };
+    };
+
+    // A basic way to implement errors as values.
+    template<typename T, typename E>
+    class result
+    {
+    public:
+        static auto success(T value) noexcept -> result<T, E>
+        {
+            return result<T, E>{ true, { .value = value } };
+        }
+
+        static auto error(E error) noexcept -> result<T, E>
+        {
+            return result<T, E>{ false, { .value = error } };
+        }
+
+        auto is_success(T& value) const noexcept -> bool
+        {
+            if (m_success)
+            {
+                value = m_union.value;
+            }
+
+            return m_success;
+        }
+
+        auto is_error(E& error) const noexcept -> bool
+        {
+            if (!m_success)
+            {
+                error = m_union.error;
+            }
+
+            return m_success;
+        }
+
+        auto to_optional() const noexcept -> std::optional<T>
+        {
+            if (m_success)
+            {
+                return std::optional<T>(m_union.value);
+            }
+            else
+            {
+                return std::optional<T>();
+            }
+        }
+
+        template<printable... S>
+        auto except(S... values) const noexcept -> T
+        {
+            if (!m_success)
+            {
+                (std::cerr << ... << values) << '\n';
+                std::terminate();
+            }
+
+            return m_union.value;
+        }
+
+        auto unwrap() const noexcept -> T
+        {
+            if (!m_success)
+            {
+                std::cerr << "result::unwrap called on error value.\n";
+                std::terminate();
+            }
+
+            return m_union.value;
+        }
+
+    private:
+        union internal_union
+        {
+            T value;
+            E error;
+        };
+
+        result(bool p_success, internal_union p_union): m_success{p_success}, m_union{p_union} {}
+
+    private:
+        bool m_success;
+
+        internal_union m_union;
+    };
+}
