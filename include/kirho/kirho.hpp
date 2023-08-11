3c29bb40e305ae3a233b868a8897b4c6c0ae4c63
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-10 21:11:22 -0400

    Fixed the is_error function. Also formatted document with clang-format.

--- include/kirho/kirho.hpp
+++ include/kirho/kirho.hpp
@@ -6,124 +6,139 @@
 
 namespace kirho
 {
-    struct empty{};
+struct empty
+{
+};
 
-    template<typename T>
-    concept printable = requires(T a)
+template <typename T>
+concept printable = requires(T a) {
     {
-        { std::cout << a };
+        std::cout << a
     };
+};
 
-    template<typename F>
-    concept deferable = requires(F a)
+template <typename F>
+concept deferable = requires(F a) {
     {
-        { a() };
+        a()
     };
+};
 
-    template<deferable F>
-    struct defer
+template <deferable F>
+struct defer
+{
+    defer(F f) noexcept : m_f(f)
     {
-        defer(F f) noexcept : m_f(f) {}
-        ~defer() noexcept { m_f(); }
-        F m_f;
-    };
-
-    template<typename Fc, typename E>
-    concept error_handler = requires(Fc f, E e)
+    }
+    ~defer() noexcept
     {
-        { f(e) };
-    };
+        m_f();
+    }
+    F m_f;
+};
 
-    // A basic way to implement errors as values.
-    template<typename T, typename E>
-    class result_t
+template <typename Fc, typename E>
+concept error_handler = requires(Fc f, E e) {
     {
-    public:
-        static auto success(T value) noexcept -> result_t<T, E>
-        {
-            return result_t<T, E>{ true, std::variant<T, E> { std::move(value) } };
-        }
-
-        static auto error(E error) noexcept -> result_t<T, E>
-        {
-            return result_t<T, E>{ false, std::variant<T, E> { std::move(error) } };
-        }
-
-        auto is_success(T& value) const noexcept -> bool
-        {
-            if (m_success)
-            {
-                value = std::get<T>(m_union);
-            }
-
-            return m_success;
-        }
-
-        auto is_error(E& error) const noexcept -> bool
-        {
-            if (!m_success)
-            {
-                error = std::get<E>(m_union);
-            }
-
-            return m_success;
-        }
-
-        auto to_optional() const noexcept -> std::optional<T>
-        {
-            if (m_success)
-            {
-                return std::optional<T>(std::get<T>(m_union));
-            }
-            else
-            {
-                return std::optional<T>();
-            }
-        }
-
-        template<printable... S>
-        auto except(S... values) const noexcept -> T
-        {
-            if (!m_success)
-            {
-                (std::cerr << ... << values) << '\n';
-                std::terminate();
-            }
-
-            return std::get<T>(m_union);
-        }
-
-        auto unwrap() const noexcept -> T
-        {
-            if (!m_success)
-            {
-                std::cerr << "result_t::unwrap called on error value.\n";
-                std::terminate();
-            }
-
-            return std::get<T>(m_union);
-        }
-
-        result_t(const result_t&) = delete;
-        result_t& operator=(const result_t&) = delete;
-
-        template<error_handler<E> F>
-        auto handle_error(F handler) const -> void
-        {
-            if (!m_success)
-            {
-                handler(m_union.error);
-            }
-        }
-
-    private:
-        result_t(bool p_success, std::variant<T, E> p_union): m_success{p_success}, m_union{p_union} {}
-
-    private:
-        bool m_success;
-
-        std::variant<T, E> m_union;
+        f(e)
     };
-}
+};
 
-#define defer(name, statement) const auto name##_defer = kirho::defer {[&]() noexcept { statement; }}; (void)name##_defer;
+// A basic way to implement errors as values.
+template <typename T, typename E>
+class result_t
+{
+  public:
+    static auto success(T value) noexcept -> result_t<T, E>
+    {
+        return result_t<T, E>{true, std::variant<T, E>{std::move(value)}};
+    }
+
+    static auto error(E error) noexcept -> result_t<T, E>
+    {
+        return result_t<T, E>{false, std::variant<T, E>{std::move(error)}};
+    }
+
+    auto is_success(T& value) const noexcept -> bool
+    {
+        if (m_success)
+        {
+            value = std::get<T>(m_union);
+        }
+
+        return m_success;
+    }
+
+    auto is_error(E& error) const noexcept -> bool
+    {
+        if (!m_success)
+        {
+            error = std::get<E>(m_union);
+        }
+
+        return !m_success;
+    }
+
+    auto to_optional() const noexcept -> std::optional<T>
+    {
+        if (m_success)
+        {
+            return std::optional<T>(std::get<T>(m_union));
+        }
+        else
+        {
+            return std::optional<T>();
+        }
+    }
+
+    template <printable... S>
+    auto except(S... values) const noexcept -> T
+    {
+        if (!m_success)
+        {
+            (std::cerr << ... << values) << '\n';
+            std::terminate();
+        }
+
+        return std::get<T>(m_union);
+    }
+
+    auto unwrap() const noexcept -> T
+    {
+        if (!m_success)
+        {
+            std::cerr << "result_t::unwrap called on error value.\n";
+            std::terminate();
+        }
+
+        return std::get<T>(m_union);
+    }
+
+    result_t(const result_t&) = delete;
+    result_t& operator=(const result_t&) = delete;
+
+    template <error_handler<E> F>
+    auto handle_error(F handler) const -> void
+    {
+        if (!m_success)
+        {
+            handler(m_union.error);
+        }
+    }
+
+  private:
+    result_t(bool p_success, std::variant<T, E> p_union)
+        : m_success{p_success}, m_union{p_union}
+    {
+    }
+
+  private:
+    bool m_success;
+
+    std::variant<T, E> m_union;
+};
+} // namespace kirho
+
+#define defer(name, statement)                                                 \
+    const auto name##_defer = kirho::defer{[&]() noexcept { statement; }};     \
+    (void)name##_defer;
